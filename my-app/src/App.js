efbc1e199ec52a6f1a6c8d7e963d2645a70c55e9
Ilya <fallout.0000@mail.ru>
2024-11-25 20:23:02 +0500

    JSXtoJS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,39 +6,50 @@ import { ReactComponent as CalenderIcon } from './calendar.svg';
 
 export const App = () => {
 	const [dateState, setDateState] = useState(new Date());
+
 	useEffect(() => {
-		setInterval(() => setDateState(new Date()), 30000);
+		const interval = setInterval(() => setDateState(new Date()), 30000);
+		return () => clearInterval(interval); // Очищаем интервал при размонтировании компонента
 	}, []);
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<CalenderIcon />
-				<p>
-					{' '}
-					{dateState.toLocaleDateString('en-GB', {
-						day: 'numeric',
-						month: 'short',
-						year: 'numeric',
-					})}
-				</p>
-				<ClockIcon />
-				<p>
-					{dateState.toLocaleString('en-US', {
-						hour: 'numeric',
-						minute: 'numeric',
-						hour12: true,
-					})}
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+
+	return React.createElement(
+		'div',
+		{ className: 'App' },
+		React.createElement(
+			'header',
+			{ className: 'App-header' },
+			React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			React.createElement(CalenderIcon),
+			React.createElement(
+				'p',
+				null,
+				' ',
+				dateState.toLocaleDateString('en-GB', {
+					day: 'numeric',
+					month: 'short',
+					year: 'numeric',
+				}),
+			),
+			React.createElement(ClockIcon),
+			React.createElement(
+				'p',
+				null,
+				dateState.toLocaleString('en-US', {
+					hour: 'numeric',
+					minute: 'numeric',
+					hour12: true,
+				}),
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+		),
 	);
 };
