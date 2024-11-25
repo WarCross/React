ff64ea46a3438904ad72f13e8f6cd362bbea6ce7
Ilya <fallout.0000@mail.ru>
2024-11-25 20:02:41 +0500

    Create Timer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ReactComponent as ClockIcon } from './clock.svg';
+import { ReactComponent as CalenderIcon } from './calendar.svg';
 
 export const App = () => {
+	const [dateState, setDateState] = useState(new Date());
+	useEffect(() => {
+		setInterval(() => setDateState(new Date()), 30000);
+	}, []);
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
+				<CalenderIcon />
 				<p>
-					Edit <code>src/App.js</code> and save to reload 2.
+					{' '}
+					{dateState.toLocaleDateString('en-GB', {
+						day: 'numeric',
+						month: 'short',
+						year: 'numeric',
+					})}
+				</p>
+				<ClockIcon />
+				<p>
+					{dateState.toLocaleString('en-US', {
+						hour: 'numeric',
+						minute: 'numeric',
+						hour12: true,
+					})}
 				</p>
 				<a
 					className="App-link"
